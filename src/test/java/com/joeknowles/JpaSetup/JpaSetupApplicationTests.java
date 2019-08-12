fc3a02d785bb444c80f7d4d1e4355325733a294b
Joe Knowles <josephbaderknowlesjr@gmail.com>
2019-08-12 16:04:08 -0500

    Initial commit

---
+++ src/test/java/com/joeknowles/JpaSetup/JpaSetupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joeknowles.JpaSetup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaSetupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
